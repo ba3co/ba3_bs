a7ca2ad08a7047857885925fe126c0dfac1594ff
Ahmed Zein <ahmedzezo1080@gmail.com>
2025-01-09 13:38:44 +0400

    KK

--- lib/features/login/ui/screens/splash_screen.dart
+++ lib/features/login/ui/screens/splash_screen.dart
@@ -1,13 +1,10 @@
 import 'package:ba3_bs/core/services/firebase/implementations/services/firestore_service.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 
 import '../../../../core/helper/extensions/getx_controller_extensions.dart';
 import '../../../../core/services/firebase/implementations/repos/filterable_datasource_repo.dart';
 import '../../../../core/services/firebase/implementations/repos/remote_datasource_repo.dart';
 import '../../../../core/services/get_x/shared_preferences_service.dart';
-import '../../../../core/services/local_database/implementations/services/hive_database_service.dart';
-import '../../../../core/services/local_database/interfaces/i_local_database_service.dart';
 import '../../../users_management/controllers/user_management_controller.dart';
 import '../../../users_management/data/datasources/roles_data_source.dart';
 import '../../../users_management/data/datasources/users_data_source.dart';
@@ -31,10 +28,7 @@ class SplashScreen extends StatelessWidget {
             ),
           );
         } else {
-          // Delay navigation until after the current frame
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            _navigateToLogin();
-          });
+          _navigateToLogin();
           return const Scaffold(
             body: Center(child: Text('Redirecting...')),
           );
@@ -56,16 +50,10 @@ class SplashScreen extends StatelessWidget {
     );
   }
 
-  Future<ILocalDatabaseService<T>> _initializeHiveService<T>({required String boxName}) async {
-    Box<T> box = await Hive.openBox<T>(boxName);
-    return HiveDatabaseService(box);
-  }
-
   void _navigateToLogin() {
-    try {
+    // Delay navigation until after the current frame
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       read<UserManagementController>().navigateToLogin();
-    } catch (e) {
-      debugPrint("Error navigating to login: $e");
-    }
+    });
   }
 }
