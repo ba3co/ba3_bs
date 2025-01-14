7cd147b5387692395d1b72233f1d5d5a0426c979
alidabol <alidabol.ae@gmail.com>
2025-01-14 12:07:13 +0400

    edite material changes_controller.dart

--- lib/features/materials/data/models/material_model.dart
+++ lib/features/materials/data/models/material_model.dart
@@ -343,8 +343,9 @@ class MaterialModel extends HiveObject implements PlutoAdaptable {
       PlutoColumn(title: 'رمز المادة', field: 'رمز المادة', type: PlutoColumnType.text()): matCode,
       PlutoColumn(title: 'الباركود', field: 'الباركود', type: PlutoColumnType.text()): matBarCode,
       PlutoColumn(title: 'التكلفة', field: 'التكلفة', type: PlutoColumnType.text()): retailPrice,
-      PlutoColumn(title: 'سعر الجملة', field: 'سعر الجملة', type: PlutoColumnType.text()): wholesalePrice,
       PlutoColumn(title: 'سعر المستهلك', field: 'سعر المستهلك', type: PlutoColumnType.text()): endUserPrice,
+
+      PlutoColumn(title: 'سعر الجملة', field: 'سعر الجملة', type: PlutoColumnType.text()): wholesalePrice,
     };
   }
 
