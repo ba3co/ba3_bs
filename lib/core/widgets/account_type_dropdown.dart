e4841101f0fd613f726f7be160eb9d151b09baca
alidabol <alidabol.ae@gmail.com>
2025-02-26 15:08:36 +0400

    feat: Refactor dropdown widgets for consistency and improved UI
    This commit refactors several dropdown widgets to enhance consistency and improve the user interface:
    
    -   **Pattern Type Dropdown:**
        -   Replaced the custom row layout with the `TextAndExpandedChildField` widget for better alignment and layout consistency.
        -   Changed dropdown field height to use `AppConstants.constHeightDropDown`.
    -   **Tax Dropdown:**
        -   Replaced the custom row layout with the `TextAndExpandedChildField` widget for better alignment and layout consistency.
        - Changed dropdown field height to use `AppConstants.constHeightDropDown`.
    -   **Account Type Dropdown:**
        -   Replaced the custom row layout with the `TextAndExpandedChildField` widget for better alignment and layout consistency.
         - Changed dropdown field height to use `AppConstants.constHeightDropDown`.
    - **Language Dropdown:**
        - Fixed the dropdown field height to `AppConstants.constHeightTextField`.

--- lib/core/widgets/account_type_dropdown.dart
+++ lib/core/widgets/account_type_dropdown.dart
@@ -1,10 +1,12 @@
 
+import 'package:ba3_bs/core/constants/app_strings.dart';
 import 'package:ba3_bs/core/helper/enums/enums.dart';
 import 'package:ba3_bs/core/interfaces/i_account_type_selection_handler.dart';
 import 'package:ba3_bs/features/floating_window/services/overlay_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../features/bill/ui/widgets/bill_shared/bill_header_field.dart';
 import '../constants/app_constants.dart';
 
 class AccountTypeDropdown extends StatelessWidget {
@@ -15,39 +17,32 @@ class AccountTypeDropdown extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: width ?? Get.width * 0.45,
-      child: Row(
-        children: [
-          const SizedBox(width: 150, child: Text('نوع الحساب')),
-          Expanded(
-            child: Container(
-              width: (Get.width * 0.45) - 100,
-              height: AppConstants.constHeightTextField,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: Colors.white,
-              ),
-              child: Obx(() {
-                return OverlayService.showDropdown<AccountType>(
-                  value: accountSelectionHandler.selectedAccountType.value,
-                  items: AccountType.values,
-                  onChanged: accountSelectionHandler.onSelectedAccountTypeChanged,
-                  textStyle: const TextStyle(fontSize: 14),
-                  itemLabelBuilder: (tax) => tax.title,
-                  height: AppConstants.constHeightTextField,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black38),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  onCloseCallback: () {
-                  },
-                );
-              }),
+    return TextAndExpandedChildField(
+label: AppStrings.accountType.tr,
+      child: Container(
+        width: (Get.width * 0.45) - 100,
+        height: AppConstants.constHeightDropDown,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.white,
+        ),
+        child: Obx(() {
+          return OverlayService.showDropdown<AccountType>(
+            value: accountSelectionHandler.selectedAccountType.value,
+            items: AccountType.values,
+            onChanged: accountSelectionHandler.onSelectedAccountTypeChanged,
+            textStyle: const TextStyle(fontSize: 14),
+            itemLabelBuilder: (tax) => tax.title,
+            height: AppConstants.constHeightTextField,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black38),
+              borderRadius: BorderRadius.circular(5),
             ),
-          ),
-        ],
+            onCloseCallback: () {
+            },
+          );
+        }),
       ),
     );
   }
