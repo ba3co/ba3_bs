ef509a9c78a6ad0556a4fce6d4e33996488fb6b2
alidabol <alidabol.ae@gmail.com>
2025-02-27 18:18:02 +0400

    feat: Improve UI, layout, and navigation features
    This commit includes several enhancements and fixes:
    
    -   **Layout:**
        -   Refactored the `PatternLayout` to remove Directionality widget and adjusted its structure and alignment.
        -   Improved the layout of `AddPatternForm` by using `FormFieldRow` and adjusting the spacing and alignment of the form fields for better organization.
    -   - Added a new component `FormFieldRow` to help on form fields layout.
    +    - Added a new component `FormFieldRow` to help on form fields layout.
        -   Removed some not needed `VerticalSpace` from `bill_details_header`.
    -   -   Updated the `gridBackgroundColor` from  `PlutoGridStyleConfig` to make it better.
    -   - Removed not need import from `user_total_delay_widget`
    +    -   Updated the `gridBackgroundColor` from  `PlutoGridStyleConfig` to make it better.
    +    - Removed not need import from `user_total_delay_widget`
    
     -   **Navigation:**
         -   Implemented floating window navigation for `AllAccountsScreen`, `AddAccountScreen`, `AllMaterialsScreen`, `AllMaterialsGroupScreen` and `AddPatternScreen`.
         -   Refactored `AccountsController`, `MaterialController`, `MaterialGroupController`, and `PatternController` to utilize floating window navigation for their respective screens.
         - Replaced direct navigation with floating window navigation in the following classes: `all_materials_group_screen`, `all_accounts_screen`, `materials_layout`, `account_layout`, `pattern_layout`.
    -  -  Refactored `StoreDropdown` to remove  `FormFieldRow` import
    -  - Removed no need import from `all_holidays_widget`.
    +    -  Refactored `StoreDropdown` to remove  `FormFieldRow` import
    +    - Removed no need import from `all_holidays_widget`.
    
     -   **Materials Group:**
         -   Fixed a minor issue in `search_material_group_text_dialog` to use await correctly.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,11 +9,13 @@ import cloud_firestore
 import file_picker
 import firebase_core
 import flutter_blue_plus_darwin
+import flutter_local_notifications
 import flutter_platform_alert
 import flutter_thermal_printer
 import geolocator_apple
 import native_dialog_plus
 import path_provider_foundation
+import printing
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -21,10 +23,12 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterPlatformAlertPlugin.register(with: registry.registrar(forPlugin: "FlutterPlatformAlertPlugin"))
   FlutterThermalPrinterPlugin.register(with: registry.registrar(forPlugin: "FlutterThermalPrinterPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   NativeDialogPlusPlugin.register(with: registry.registrar(forPlugin: "NativeDialogPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
